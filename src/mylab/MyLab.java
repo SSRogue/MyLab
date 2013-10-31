5f80da19477ab7d36982119fe8b4f3fcff08f652
chen203g <chen203g@142.3.27.184>
2013-10-31 09:28:36 -0600

    All files in the project are added to the repository

---
+++ src/mylab/MyLab.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mylab;
+
+/**
+ *
+ * @author chen203g
+ */
+public class MyLab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
