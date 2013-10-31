018cbaaad662b35c426d225c8b1adc1219f80997
chen203g <chen203g@142.3.27.184>
2013-10-31 09:30:24 -0600

    All files in the project are added to the repository

--- src/mylab/MyLab.java
+++ src/mylab/MyLab.java
@@ -15,5 +15,7 @@ public class MyLab {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("New line added");
     }
 }
